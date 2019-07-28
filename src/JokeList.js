a154389d517a3668b54026b12d890835e625761d
ZoloMohan <mohanzolomon@gmail.com>
2019-07-29 02:25:07 +0530

    Jokes List Styling

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,34 +1,41 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import './JokeList.css';
 
 export default class JokeList extends Component {
 
-	static defaultProps = {
-		defaultNoOfJokes: 10
-	}
-	
-	constructor(props){
-		super(props);
-		this.state = {
-			jokes: []
-		}
-	}
+  static defaultProps = {
+    defaultNoOfJokes: 100
+  }
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      jokes: []
+    }
+  }
 
-	async componentDidMount(){
-		let jokes = [];
-		while (jokes.length < this.props.defaultNoOfJokes){
-			let jokeResponse = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
-			jokes.push(jokeResponse.data.joke);
-		}
-		this.setState({jokes: jokes})
-	}
+  async componentDidMount(){
+    let jokes = [];
+    while (jokes.length < this.props.defaultNoOfJokes){
+      let jokeResponse = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
+      jokes.push(jokeResponse.data.joke);
+			this.setState({jokes: jokes})
+    }
+  }
 
   render() {
-    return (
-      <div>
-        <h1>Dad Jokes</h1>
-				{this.state.jokes.map(joke => <div>{joke}</div>)}
-      </div>
-    )
+  return (
+    <div className='JokeList'>
+			<div className='JokeList-sidebar'>
+    		<h1 className='JokeList-title'><span>Dad</span> Jokes</h1>
+				<img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+				<button className='JokeList-getmore'>Fetch Jokes</button>
+			</div>
+			<div className='JokeList-jokes'>
+      	{this.state.jokes.map(joke => <div>{joke}</div>)}
+			</div>
+    </div>
+  )
   }
 }
