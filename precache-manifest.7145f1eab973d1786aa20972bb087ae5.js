9a1a9879658a2e711f63f506de1e30d86878e687
ZoloMohan <mohanzolomon@gmail.com>
2019-07-29 15:32:44 +0530

    Updates

---
+++ precache-manifest.7145f1eab973d1786aa20972bb087ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9559a162c9cfdfeeaaa3866cc7f1ce9f",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "388378108fdd6813ea0e",
+    "url": "/dad-jokes/static/css/main.b756211f.chunk.css"
+  },
+  {
+    "revision": "8c0850ec13ca45a99058",
+    "url": "/dad-jokes/static/js/2.af65b730.chunk.js"
+  },
+  {
+    "revision": "388378108fdd6813ea0e",
+    "url": "/dad-jokes/static/js/main.bb355aa7.chunk.js"
+  },
+  {
+    "revision": "03aa9e7745c6e7ee15e7",
+    "url": "/dad-jokes/static/js/runtime~main.c81b4217.js"
+  }
+]);
